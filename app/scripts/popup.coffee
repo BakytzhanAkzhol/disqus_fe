25d43bc4b57c7f10c82f1197554ea512962532a1
Bakytzhan <bakytzhan.akzhol@ce.sdu.edu.kz>
2015-04-30 05:28:44 +0600

    Начинаю работать с CoffeScript

--- app/scripts/popup.coffee
+++ app/scripts/popup.coffee
@@ -1,3 +1,17 @@
 'use strict';
-
 # this script is used in popup.html
+ractive = new Ractive
+      el : '#container'
+      template : '#template'
+      data :
+        user: 'Bakytzhan'
+        email:'akzhol_b@bk.ru'
+        comment:'I told him!I win!'
+        avatar:'205e460b479e2e5b48aec07710c08d50'
+    
+   
+
+ractive.on 'activate', =>
+  key = md5 'message'
+
+  ractive.set 'avatar',key
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,24 +1,24 @@
 (function() {
   'use strict';
+  var ractive;
 
-var ractive = new Ractive({
-      // The `el` option can be a node, an ID, or a CSS selector.
-      el: '#container',
-
-      // We could pass in a string, but for the sake of convenience
-      // we're passing the ID of the <script> tag above.
-      template: '#template',
-
-      // Here, we're passing in some initial data
-      data: {
-        user: 'Bakytzhan',
-      email:'akzhol_b@bk.ru',
-      comment:'I told him!I win!',
-      avatar:'205e460b479e2e5b48aec07710c08d50?s=48'
+  ractive = new Ractive({
+    el: '#container',
+    template: '#template',
+    data: {
+      user: 'Bakytzhan',
+      email: 'akzhol_b@bk.ru',
+      comment: 'I told him!I win!',
+      avatar: '205e460b479e2e5b48aec07710c08d50'
     }
-    });
+  });
 
-ractive.on( 'activate', function ( event ) {
-  alert( 'Activating!' );
-});
+  ractive.on('activate', (function(_this) {
+    return function() {
+      var key;
+      key = md5('message');
+      return ractive.set('avatar', key);
+    };
+  })(this));
+
 }).call(this);
